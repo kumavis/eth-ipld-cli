8361ef6915c643f92f75a5dba6a17f77d49e0d5e
kumavis <aaron@kumavis.me>
2017-07-13 13:03:17 -0700

    rpc - check specified type

--- commands/rpc.js
+++ commands/rpc.js
@@ -1,15 +1,14 @@
 'use strict'
 const concatStream = require('mississippi').concat
-const cidForHash = require('ipld-eth-block/src/common').cidForHash
 const getStdin = require('get-stdin')
-const EthBlockHeader = require('ethereumjs-block/header')
+const ethBlockHeaderFromRpc = require('ethereumjs-block/header-from-rpc')
 const ethUtil = require('ethereumjs-util')
 
 
 module.exports = {
   command: 'rpc [type]',
 
-  description: 'Generate CIDs for Ethereum hashes',
+  description: 'Parse rpc result into binary',
 
   // dont parse hash as Number '_'
   builder: (yargs) => {
@@ -26,14 +25,20 @@ module.exports = {
     if (process.stdin.isTTY) {
       throw new Error('Expected stdin.')
     } else {
+      const type = argv.type
       getStdin()
       .then((resJson) => {
-        let blockJson = JSON.parse(resJson)
-        if (argv.result) blockJson = blockJson.result
-        const blockHeader = rpcToBlockHeader(blockJson)
-        const rawBlock = blockHeader.serialize()
-        // const cid = cidForHash('eth-block', ethUtil.toBuffer(block.hash))
-        process.stdout.write(rawBlock)
+        switch (type) {
+          case 'block':
+            let blockJson = JSON.parse(resJson)
+            if (argv.result) blockJson = blockJson.result
+            const blockHeader = ethBlockHeaderFromRpc(blockJson)
+            const rawBlock = blockHeader.serialize()
+            process.stdout.write(rawBlock)
+            return
+          default:
+            throw new Error(`Unsupported rpc type "${type}"`)
+        }
       })
       .catch((err) => {
         console.error(err)
@@ -41,24 +46,4 @@ module.exports = {
     }
 
   }
-}
-
-function rpcToBlockHeader(block){
-  const blockHeader = new EthBlockHeader()
-  blockHeader.parentHash = block.parentHash
-  blockHeader.uncleHash = block.uncleHash || block.sha3Uncles
-  blockHeader.coinbase = block.coinbase || block.miner
-  blockHeader.stateRoot = block.stateRoot
-  blockHeader.transactionsTrie = blockHeader.transactionsTrie || block.transactionsRoot
-  blockHeader.receiptTrie = block.receiptTrie || block.receiptRoot || block.receiptsRoot || ethUtil.SHA3_NULL
-  blockHeader.bloom = block.bloom || block.logsBloom
-  blockHeader.difficulty = block.difficulty
-  blockHeader.number = block.number
-  blockHeader.gasLimit = block.gasLimit
-  blockHeader.gasUsed = block.gasUsed
-  blockHeader.timestamp = block.timestamp
-  blockHeader.extraData = block.extraData
-  blockHeader.mixHash = block.mixHash
-  blockHeader.nonce = block.nonce
-  return blockHeader
 }
